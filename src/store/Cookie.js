c17310f7fe4a5bdb924e52ed787a9d0b260d3960
ThangMai81 <maithang18122003@gmail.com>
2025-05-20 17:25:59 +0700

    Change the way to get cookie

--- src/store/Cookie.js
+++ src/store/Cookie.js
@@ -1,9 +1,9 @@
 export function getCookie(name) {
   const cookies = document.cookie.split("; ");
   for (const c of cookies) {
-    const [key, value] = c.split("=");
-    console.log("value?: ", value);
-    if (key === name) return value;
+    const [key, ...rest] = c.split("=");
+    const value = rest.join("=");
+    if (key === name) return decodeURIComponent(value);
   }
   return null;
 }
